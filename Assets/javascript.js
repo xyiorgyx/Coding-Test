e05b147de29651921ec458e59bd3edce25b0916c
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-10-31 08:41:57 -0400

    Boiler plate and questions
    added boiler plate and 5 questions to the repository. starter code.

---
+++ Assets/javascript.js
@@ -0,0 +1,93 @@
+
+var testQuestions = [
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    }, 
+    {
+        question: 'In CSS, "justify content" minipulates the spacing of content on what axis?',
+        choices: {
+            a: 'Cross Axis',
+            b: 'Main Axis',
+            c: 'All Axis'
+        },
+        answer: 'b'
+    },
+    {
+        question: 'What CSS attribute allows a programmer to input data using a URL?',
+        choices: {
+            a: 'src = ""',
+            b: '<a>',
+            c: 'href = ""'
+        },
+        answer: 'c'
+    },
+    {
+        question: 'Which one of these choices is ',
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },    
+];
---
+++ javascript.js
@@ -0,0 +1,93 @@
+
+var testQuestions = [
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing',
+        },
+        answer: 'a'
+    }, 
+    {
+        question: 'In CSS, "justify content" minipulates the spacing of content on what axis?',
+        choices: {
+            a: 'Cross Axis',
+            b: 'Main Axis',
+            c: 'All Axis'
+        },
+        answer: 'b'
+    },
+    {
+        question: 'What CSS attribute allows a programmer to input data using a URL?',
+        choices: {
+            a: 'src = ""',
+            b: '<a>',
+            c: 'href = ""'
+        },
+        answer: 'c'
+    },
+    {
+        question: 'Which one of these choices is a self closing tag?',
+        choices: {
+            a: '<Button>',
+            b: '<a>',
+            c: '<hr>'
+        },
+        answer: 'c'
+    },
+    {
+        question: 'What is the main purpose of Meta Tags?',
+        choices: {
+            a: 'Used in conjunction with Zuckerburg\'s Metaverse?',
+            b: 'Stores information specifically about the webpage',
+            c: 'There is no such thing as a meta tage'
+        },
+        answer: 'b'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },
+    {
+        question: "What is the primary use for CSS code?",
+        choices: {
+            a: 'Styling',
+            b: 'Structuring',
+            c: 'Bug fixing'
+        },
+        answer: 'a'
+    },    
+];

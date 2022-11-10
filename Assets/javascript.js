3932c3acdba32774528854e1c21822608cd97ed6
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-11-09 23:33:36 -0500

    fixed javascriipt
    fixed javascript not presenting the question value.

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -8,7 +8,7 @@ var testQuestions = [
             c: 'Bug fixing',
             d: 'Debugging'
         },
-        answer: 'a'
+        answer: 'Styling'
     },
     {
         question: 'In CSS, "justify content" minipulates the spacing of content on what axis?',
@@ -18,25 +18,23 @@ var testQuestions = [
             c: 'All Axis'
 
         },
-        answer: 'b'
+        answer: 'Main Axis'
     },
 ];
 
 
 var numCorrect = 0;
-var userAnswer = '';
 var secondsLeft = 100;
 var questionNum = 0;
-var answer = testQuestions[questionNum].answer
 var currentQuestion = document.getElementById("question");
+var answer = testQuestions[questionNum].answer;
 var choice1 = document.getElementById('a');
 var choice2 = document.getElementById('b');
 var choice3 = document.getElementById('c');
 var choice4 = document.getElementById('d');
 var timer = document.getElementById('timer');
-function displaymessage(){
-
-}
+// function displaymessage(){
+// }
 
 function clock() {
     var timerInterval = setInterval(function () {
@@ -51,54 +49,84 @@ function clock() {
     }, 1000);
 }
 
-clock()
+clock();
 function displayQuestion() {
-    currentQuestion.innerHTML = testQuestions[questionNum].question;
-    choice1.innerHTML = testQuestions[questionNum].choices.a;
-    choice2.innerHTML = testQuestions[questionNum].choices.b;
-    choice3.innerHTML = testQuestions[questionNum].choices.c;
-    choice4.innerHTML = testQuestions[questionNum].choices.d;
-    console.log(testQuestions[questionNum].question)
-}
-
-function changeGreen() {
-    currentTarget.setAttribute(
-        "style",
-        "background-color: green"
-    );
-}
-
-function changeRed() {
-    currentTarget.setAttribute(
-        "style",
-        "background-color: red"
-    );
-}
-
-displayQuestion()
-function checkAnswer() {
-    for (var i=0, i < questions.length; i++ )
-    userAnswer = (choices[i].querysSelector('input))
-    if (testQuestions[questionNum].answer == userAnswer) {
-        changeGreen();
-
+    for (i=0; i < 4; i++){
+        currentQuestion.textContent = testQuestions[questionNum].question;
+        choice1.textContent = testQuestions[questionNum].choices.a;
+        choice2.textContent = testQuestions[questionNum].choices.b;
+        choice3.textContent = testQuestions[questionNum].choices.c;
+        choice4.textContent = testQuestions[questionNum].choices.d;
+        
     }
-    else if (testQuestions[questionNum].answer !== userAnswer) {
-        changeRed();
 
-    }
 }
+
 
 
 displayQuestion()
-choice1.addEventListener('click', checkAnswer);
-choice2.addEventListener('click', checkAnswer);
-choice3.addEventListener('click', checkAnswer);
-choice4.addEventListener('click', checkAnswer);
+
+choice1.addEventListener('click', function() {
+    if (choice1.textcontent = answer){
+        choice1.setAttribute(
+            "style",
+            "background-color: green"
+        );
+        
+    } else {
+        choice1.setAttribute(
+            "style",
+            "background-color: red"
+        );
+    }
+    questionNum++;
+});
+choice2.addEventListener('click', function() {
+    if (choice2.textcontent === answer){
+        choice2.setAttribute(
+            "style",
+            "background-color: green"
+        );
+        
+    } else {
+        choice2.setAttribute(
+            "style",
+            "background-color: red"
+        );
+    }
+});
+choice3.addEventListener('click', function() {
+    if (choice3.textcontent === answer){
+        choice3.setAttribute(
+            "style",
+            "background-color: green"
+        );
+        
+    } else {
+        choice3.setAttribute(
+            "style",
+            "background-color: red"
+        );
+    }
+});
+choice4.addEventListener('click', function() {
+    if (choice4.textcontent === answer){
+        choice4.setAttribute(
+            "style",
+            "background-color: green"
+        );
+        
+    } else {
+        choice4.setAttribute(
+            "style",
+            "background-color: red"
+        );
+    }
+});
 // nextbtn.addEventListener('click', next)
 
 var correctMess = 'Thats correct';
 var incorrectMess = 'sorry, thats not correct, the answer is: '
-
-
-console.log(secondsLeft)
+console.log(answer)
+console.log(choice1.textContent)
+console.log(choice2.textContent)
